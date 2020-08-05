63bb7a8499d3b6e58e6accba736cb0974d77cb48
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-06 09:38:49 +1200

    Update WeaponCard.java

--- A1/src/WeaponCard.java
+++ A1/src/WeaponCard.java
@@ -1,3 +1,27 @@
 class WeaponCard implements Card {
-    WeaponType weapon = null;
+    WeaponType weapon;
+
+    public WeaponCard(WeaponType weapon){
+        this.weapon = weapon;
+    }
+
+
+
+    public void setWeapon(WeaponType weapon){
+        this.weapon = weapon;
+
+    }
+
+    public WeaponType getWeapon(){
+        return this.weapon;
+    }
+
+    /* Deletes selected weapon */
+    public void delete(){
+
+    }
+
+
+
+
 }
