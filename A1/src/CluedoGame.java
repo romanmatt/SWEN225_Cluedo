04fd2693595e4c5499c7ebce8ddac55f400aa20e
julfran <68882531+julfran@users.noreply.github.com>
2020-08-05 15:37:39 +1200

    Add files via upload

--- A1/src/CluedoGame.java
+++ A1/src/CluedoGame.java
@@ -1,14 +1,52 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CluedoGame {
-    public static int numOfPlayers;
     private static boolean isGameOver;
-    private Board boardGame;
+    private static Board board = new Board();
     private Player player;
     public int numOfSteps;
 
-    public void run() {
+
+    ArrayList<Card> allDeckCards = new ArrayList<Card>();
+    ArrayList<Player> activePlayers = new ArrayList<Player>();
+    
+    /**
+     * Make two variable that randomly generates
+     * numbers between 1-6 and add them together to get
+     * sum total
+     * @ return dieSum
+     * */
+    public int rollDiceGenerator() {
+        int die1 = (int) (Math.random()*6) +1;
+        int die2 = (int) (Math.random()*6) +1;
+        int dieSum = die1 + die2;
+        return dieSum;
     }
 
-    public int rollDiceGenerator() {
-        return 0;
+    /**
+     * introGame() is a method that asks users how many players are
+     * playing.
+     * */
+    public static String userInput() {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        try{
+            System.out.println("CLUEDO: TEXT-BASED GUI DESKTOP VERSION.\n" +
+                    "PLEASE ENTER INITIALS OF PLAYERS BETWEEN [3-6] FOR THE GAME: \n" +
+                    "INPUT EXAMPLE: \"M W G P\""
+            );
+            input = sc.nextLine();
+        }
+        catch(Error e){
+            System.out.println("ERROR: " + e);
+        }
+        return input;
     }
+
+
+
+
+
 }
