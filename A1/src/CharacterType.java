3ae88eb6219355f48779e5f6ea3b2dcb6177c7fc
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-06 09:28:24 +1200

    Update CharacterType.java

--- A1/src/CharacterType.java
+++ A1/src/CharacterType.java
@@ -1,8 +1,8 @@
 public enum CharacterType {
-    MissScarlett,
-    ColonelMustard,
-    MrsWhite,
-    MrGreen,
-    MrsPeacock,
-    ProfessorPlum
+    MissScarlett {public String toString(){return "Miss Scarlett";}} ,
+    ColonelMustard {public String toString(){return "Colonel Mustard ";}} ,
+    MrsWhite {public String toString(){return "Mrs. White";}} ,
+    MrGreen {public String toString(){return "Mr. Green";}} ,
+    MrsPeacock {public String toString(){return "Mrs. Peacock";}} ,
+    ProfessorPlum  {public String toString(){return "Professor Plum";}}
 }
