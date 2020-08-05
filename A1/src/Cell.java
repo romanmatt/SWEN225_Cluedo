f0345a45a3e1f1dc9c9ee3951efcfaa664c41924
LoadingReadyWalk <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-05 12:30:09 +1200

    Update Cell.java
    Added x and y fields and default getter methods

--- A1/src/Cell.java
+++ A1/src/Cell.java
@@ -1,4 +1,14 @@
 public interface Cell {
+    private int x;
+    private int y;
     int getXPos = 0;
     int getYPos = 0;
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
 }
