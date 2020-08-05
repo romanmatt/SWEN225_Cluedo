fd634d306b6af75609eadae9262c1f4140e1241e
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-05 16:23:16 +1200

    Update Room.java

--- A1/src/Room.java
+++ A1/src/Room.java
@@ -1,3 +1,53 @@
 class Room {
-
+  private ArrayList<Cell> cells; // The cells of the board that make up this room
+  private ArrayList<Player> players; // The players currently in this room 
+  private ArrayList<Weapon> weapons; // The weapon piece currently in this room
+  
+  public ArrayList<Cell> getCells() {
+    return cells;
+  }
+  
+  public ArrayList<Player> getPlayers() {
+    return players;
+  }
+  
+  public ArrayList<Weapon> getWeapons() {
+    return weapons;
+  }
+  
+  public Boolean containsWeapon(Weapon weapon) {
+    if (weapons.contains(weapon)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public Boolean containsPlayer(Player player) {
+    if (players.contains(player)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public void placePlayer(Player player) {
+    players.add(player);
+  }
+  
+  public void placeWeapon(Weapon weapon) {
+    weapons.add(weapon);
+  }
+  
+  public void removePlayer(Player player) {
+    if (players.contains(player)) {
+      players.remove(player);
+    }
+  }
+  
+  public void removeWeapon(Weapon weapon) {
+    if (weapons.contains(weapon)) {
+      weapons.remove(weapon);
+    }
+  }
 }
