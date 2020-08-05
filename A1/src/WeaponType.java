239bdfd547ebf88cb07687bbb25aaddc9356a070
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-05 16:31:59 +1200

    Update WeaponType.java

--- A1/src/WeaponType.java
+++ A1/src/WeaponType.java
@@ -1,8 +1,12 @@
+
+
 public enum WeaponType {
-    CandleStick,
-    Dagger,
-    LeadPipe,
-    Revolver,
-    Rope,
-    Spanner
+    CandleStick {public String toString(){return "Candlestick";}} ,
+    Dagger {public String toString(){return "Dagger";}} ,
+    LeadPipe {public String toString(){return "Lead Pipe";}} ,
+    Revolver {public String toString(){return "Revolver";}} ,
+    Rope {public String toString(){return "Rope";}} ,
+    Spanner  {public String toString(){return "Spanner";}}
 }
+
+
