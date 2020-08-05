b2650d832fc4bb3a85e4cf3c318bc9f94930f50e
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-05 12:41:15 +1200

    Update Board.java

--- A1/src/Board.java
+++ A1/src/Board.java
@@ -5,4 +5,12 @@ public class Board {
     public final String draw() {
         return null;
     }
+    
+    // Checks if a player can make their desired move
+    // First cell is the one the player is standing on
+    // Second cell is the one they wish to move to
+    // The roll is the number they rolled on the dice
+    public Boolean canMove(Cell start, Cell finish, int roll) {
+        return false;
+    }
 }
