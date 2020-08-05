056f526a950a59c7cb805e6edab5691684548f6f
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-06 09:28:49 +1200

    Update CharacterCard.java

--- A1/src/CharacterCard.java
+++ A1/src/CharacterCard.java
@@ -1,3 +1,20 @@
-class CharacterCard implements Card {
-    CharacterType character = null;
+public class CharacterCard implements Card {
+    CharacterType character;
+
+    /* Constructor */
+    public CharacterCard(CharacterType character) {
+        this.character = character;
+    }
+
+
+    public CharacterType getCharacter() {
+        return this.character;
+    }
+
+    /* Deletes selected character */
+    public void delete(){
+
+    }
+
+
 }
