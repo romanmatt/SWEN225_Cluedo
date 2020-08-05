c56e92fa2a63f3fd7a99f0ca1b8a1f10f9ad8a46
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-05 17:21:26 +1200

    Update Player.java

--- A1/src/Player.java
+++ A1/src/Player.java
@@ -1,7 +1,43 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Player {
-    private Character playersCharacter;
+    private CharacterType playersCharacter;
     private RoomCell playersRoom;
-    private ArrayList <Card> playersCardStack ;
+    private boolean isStillPlaying;
+    private HashSet <Card> playersHand = new HashSet<>();
+    String notes = ""; //Any extra notes for the player
+
+    /* Constructor for the Player object */
+    public Player(CharacterType character){
+        this.playersCharacter = character;
+        isStillPlaying = true;
+    }
+
+
+    //Not needed but will remain just in case
+
+    /**
+     * Used for removing a player from the game loop when an accusation is incorrect
+     * @param isStillPlaying
+     */
+    public void isStillPlaying(boolean isStillPlaying){
+        this.isStillPlaying = isStillPlaying;
+    }
+
+    public boolean getIsStillPlaying(){
+        return this.isStillPlaying;
+    }
+
+
+
+
+
+
+
+
+
 }
+
+
+
