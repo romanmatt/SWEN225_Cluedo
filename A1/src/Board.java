53c2cf463eb65ff8c117d37b40ed291051a4a3d7
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-05 12:43:02 +1200

    Update Board.java

--- A1/src/Board.java
+++ A1/src/Board.java
@@ -7,10 +7,10 @@ public class Board {
     }
     
     // Checks if a player can make their desired move
-    // First cell is the one the player is standing on
-    // Second cell is the one they wish to move to
-    // The roll is the number they rolled on the dice
-    public Boolean canMove(Cell start, Cell finish, int roll) {
+    // player is the player wishing to move. Their position on the board is taken from this input
+    // finish is the cell they wish to end their move on
+    // roll is the number they rolled on the dice
+    public Boolean canMove(Player player, Cell finish, int roll) {
         return false;
     }
 }
