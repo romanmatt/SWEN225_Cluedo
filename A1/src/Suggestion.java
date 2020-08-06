e4bd0aa018c3a60376654275908c239f00e6068a
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-06 14:43:44 +1200

    Update Suggestion.java

--- A1/src/Suggestion.java
+++ A1/src/Suggestion.java
@@ -1,8 +1,29 @@
+import java.util.*;
+
 public class Suggestion {
-    public RoomType suggestedRoom;
-    public WeaponType suggestedWeapon;
-    public CharacterType suggestedCharacter;
-    public String toString() {
-        return null;
+    
+    //Temporary 
+    
+//    public RoomType suggestedRoom;
+//    public WeaponType suggestedWeapon;
+//    public CharacterType suggestedCharacter;
+//    public String toString() {
+//        return null;
+//    }
+
+    private Card[] suggestion;
+    private Player currentPlayer;
+    private ArrayList<Player> listOfPlayers;
+    private Scanner scanner;
+
+    /* Constructor for Suggestion object */
+    public Suggestion(Card[] guesses, Player player, ArrayList<Player> listOfPlayers){
+        this.suggestion = guesses;
+        this.currentPlayer = player;
+        this.listOfPlayers = listOfPlayers;
+        scanner = new Scanner(System.in);
+
+
     }
+
 }
