3fd19d978a219ae2731f055eca15d9f6398bc531
julfran <68882531+julfran@users.noreply.github.com>
2020-08-06 21:10:14 +1200

    Update CluedoGame.java

--- A1/src/CluedoGame.java
+++ A1/src/CluedoGame.java
@@ -4,16 +4,63 @@ import java.util.Scanner;
 
 public class CluedoGame {
     private static boolean isGameOver;
+    public int numOfSteps;
     private static Board board = new Board();
     private Player player;
-    public int numOfSteps;
 
+    private static String [] characterList = {"W: Mrs. White",
+            "S: Miss Scarlett", "M: Colonel Mustard",
+            "P: Professor Plum", "C: Mrs. Peacock",
+            "G: Mr. Green"};
+    private static String [] weaponList = {""};
+    private static String [] roomList = {""};
 
     ArrayList<Card> allDeckCards = new ArrayList<Card>();
     ArrayList<Player> activePlayers = new ArrayList<Player>();
-    
+    ArrayList<Card> playerDeck = new ArrayList<Card>();
+    ArrayList<Card> solutionDeck = new ArrayList<Card>();
+
+
     /**
-     * Make two variable that randomly generates
+     * run() is a collective of methods
+     * responsible for the text GUI of the game
+     * */
+    public void run(){
+        userInput();
+    }
+
+    /**
+     * userInput() takes care of the user's text input
+     * on choice of number of players, what player they wish to play
+     * */
+    private int userInput() {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        try{
+            System.out.println("CLUEDO: TEXT-BASED GUI DESKTOP VERSION.\n" +
+                    "PLEASE ENTER NUMBER OF PLAYERS BETWEEN [3-6] FOR THE GAME: \n" +
+                    "INPUT EXAMPLE: 4 "
+            );
+            num = sc.nextInt();
+            if(num<3 || num>6 ){
+                throw new IllegalArgumentException("NOT A VALID NUMBER! PLEASE ENTER NUMBER FROM 3-6.");
+            }
+        }
+        catch(Error e) {
+            System.out.println("ERROR: " + e);
+        }
+
+
+        }
+
+
+
+
+
+
+
+    /**
+     * Make two die variable that randomly generates
      * numbers between 1-6 and add them together to get
      * sum total
      * @ return dieSum
@@ -25,27 +72,10 @@ public class CluedoGame {
         return dieSum;
     }
 
-    /**
-     * introGame() is a method that asks users how many players are
-     * playing.
-     * */
-    public static String userInput() {
-        Scanner sc = new Scanner(System.in);
-        String input = "";
-        try{
-            System.out.println("CLUEDO: TEXT-BASED GUI DESKTOP VERSION.\n" +
-                    "PLEASE ENTER INITIALS OF PLAYERS BETWEEN [3-6] FOR THE GAME: \n" +
-                    "INPUT EXAMPLE: \"M W G P\""
-            );
-            input = sc.nextLine();
-        }
-        catch(Error e){
-            System.out.println("ERROR: " + e);
-        }
-        return input;
+    public static void main(String[] args){
+        CluedoGame cluedoGame = new CluedoGame();
+        cluedoGame.run();
     }
-
-
 
 
 
