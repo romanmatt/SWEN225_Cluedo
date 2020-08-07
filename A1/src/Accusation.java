ac1ec01744f4e807cb6ed84ebcfc6127260b042c
romanmatt <68979276+romanmatt@users.noreply.github.com>
2020-08-07 16:25:58 +1200

    Update Accusation.java

--- A1/src/Accusation.java
+++ A1/src/Accusation.java
@@ -3,6 +3,29 @@ public class Accusation {
     public WeaponType accusedWeapon;
     public CharacterType accusedCharacter;
 
+
+    /* Constructor for Accusation object */
+    public Accusation(RoomType accusedRoom, WeaponType accusedWeapon, CharacterType accusedCharacter){
+        this.accusedRoom = accusedRoom;
+        this.accusedWeapon = accusedWeapon;
+        this.accusedCharacter = accusedCharacter;
+    }
+
+
+public boolean checkAccusation(Player player){
+        for(int i = 0; i < 6; i++){
+            System.out.println(); //temporary
+
+            //if the accused weapon, character and room matches the murderer's
+            //game stops
+            //player can no longer play
+        }
+
+
+        return false;
+}
+
+
     public String toString() {
         return null;
     }
