414834ad8e64b0ccf3f0fc4ebaab46f179c4be69
julfran <68882531+julfran@users.noreply.github.com>
2020-08-08 16:57:10 +1200

    Update DoorCell.java

--- A1/src/DoorCell.java
+++ A1/src/DoorCell.java
@@ -1,21 +1,21 @@
-
 public class DoorCell implements Cell {
 
-	private int x;
-	private int y;
-	private Room room;
-	
-	public DoorCell(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
+    private int x;
+    private int y;
+    private Room room;
 
+    public DoorCell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getXPos() {
+        return x;
+    }
+
+    @Override
+    public int getYPos() {
+        return y;
+    }
 }
