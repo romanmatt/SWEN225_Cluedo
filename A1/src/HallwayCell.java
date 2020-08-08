acbeb677a8d7c02d761d0bbbba5c9e4e198c265e
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-08 15:56:16 +1200

    Update HallwayCell.java

--- A1/src/HallwayCell.java
+++ A1/src/HallwayCell.java
@@ -1,4 +1,37 @@
+
 public class HallwayCell implements Cell {
-    public int xPos;
-    public int yPos;
+
+	private int x;
+	private int y;
+	private Player player = null;
+	
+	public HallwayCell(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public boolean isOccupied() {
+		if (player != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Player getPlayer() {
+		if (isOccupied()) {
+			return player;
+		} else {
+			return null;
+		}
+	}
+	
 }
