903ec328f9fe60f8a9830599d36ae40639b65b7b
julfran <68882531+julfran@users.noreply.github.com>
2020-08-08 16:03:31 +1200

    Update Room.java

--- A1/src/Room.java
+++ A1/src/Room.java
@@ -1,62 +1,64 @@
+import java.util.ArrayList;
+
 class Room {
-  private ArrayList<Cell> cells; // The cells of the board that make up this room
-  private ArrayList<Cell> doors; // The door cells this room uses for movement calculations 
-  private ArrayList<Character> characters; // The players currently in this room 
-  private ArrayList<Weapon> weapons; // The weapon piece currently in this room
-  
-  public ArrayList<Cell> getCells() {
-    return cells;
-  }
-  
-  public ArrayList<Cell> getDoors() {
-    return doors;
-  }
-  
-  public void addCell(RoomCell cell) {
-		cells.add(cell);
-	}
-  
-  public ArrayList<Character> getCharacters() {
-    return characters;
-  }
-  
-  public ArrayList<Weapon> getWeapons() {
-    return weapons;
-  }
-  
-  public Boolean containsWeapon(Weapon weapon) {
-    if (weapons.contains(weapon)) {
-      return true;
-    } else {
-      return false;
+    private ArrayList<Cell> cells; // The cells of the board that make up this room
+    private ArrayList<Cell> doors; // The door cells this room uses for movement calculations
+    private ArrayList<Character> characters; // The players currently in this room
+    private ArrayList<WeaponType> weapons; // The weapon piece currently in this room
+
+    public ArrayList<Cell> getCells() {
+        return cells;
     }
-  }
-  
-  public Boolean containsCharacter(Character character) {
-    if (characters.contains(character)) {
-      return true;
-    } else {
-      return false;
+
+    public ArrayList<Cell> getDoors() {
+        return doors;
     }
-  }
-  
-  public void placeCharacter(Character character) {
-    characters.add(character);
-  }
-  
-  public void placeWeapon(Weapon weapon) {
-    weapons.add(weapon);
-  }
-  
-  public void removeCHaracter(Character character) {
-    if (characters.contains(charatcer)) {
-      characters.remove(character);
+
+    public void addCell(RoomCell cell) {
+        cells.add(cell);
     }
-  }
-  
-  public void removeWeapon(Weapon weapon) {
-    if (weapons.contains(weapon)) {
-      weapons.remove(weapon);
+
+    public ArrayList<Character> getCharacters() {
+        return characters;
     }
-  }
+
+    public ArrayList<WeaponType> getWeapons() {
+        return weapons;
+    }
+
+    public Boolean containsWeapon(WeaponType weapon) {
+        if (weapons.contains(weapon)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean containsCharacter(Character character) {
+        if (characters.contains(character)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void placeCharacter(Character character) {
+        characters.add(character);
+    }
+
+    public void placeWeapon(WeaponType weapon) {
+        weapons.add(weapon);
+    }
+
+    public void removeCharacter(Character character) {
+        if (characters.contains(character)) {
+            characters.remove(character);
+        }
+    }
+
+    public void removeWeapon(WeaponType weapon) {
+        if (weapons.contains(weapon)) {
+            weapons.remove(weapon);
+        }
+    }
 }
