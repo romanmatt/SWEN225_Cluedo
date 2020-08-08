881dfb661f5021a75fd1da4153dcc1ddeb97649d
riverarive <38926963+riverarive@users.noreply.github.com>
2020-08-08 17:00:04 +1200

    Update RoomCard.java

--- A1/src/RoomCard.java
+++ A1/src/RoomCard.java
@@ -1,13 +1,43 @@
+/**
+ * Room cards
+ * Contains room card methods
+ */
 class RoomCard implements Card {
     RoomType room = null;
     
+    /** Constructor */
     public RoomCard (RoomType room){
         this.room = room;   
     }
     
+    /** Setter */
+    public RoomType setRoom (RoomType room){
+        this.room = room;   
+    }
+    
+    /** Getter */
     public RoomType getRoom() {
         return this.room;
     }
-    
-    
+
+    /** Remove */
+    public RoomType removeRoom() {
+        this.room = null;
+    }
+
+    /** Equals */
+    public boolean equals(Object o){
+        //checking if o is null
+        if(o == null){
+            return false;
+        }
+
+        //checking if o is equal to this class
+        if(getClass() != o.getClass()){
+            return false;
+        }
+
+        CharacterCard other = (CharacterCard) o;
+        return this.character == other.character;
+    }
 }
