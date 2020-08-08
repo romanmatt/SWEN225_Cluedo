767252e4a190abdf78388c4cd83896e2dc9f6623
riverarive <38926963+riverarive@users.noreply.github.com>
2020-08-08 17:10:12 +1200

    Update WeaponCard.java

--- A1/src/WeaponCard.java
+++ A1/src/WeaponCard.java
@@ -1,27 +1,43 @@
+/**
+ * Weapon cards
+ * Contains weapon card methods
+ */
 class WeaponCard implements Card {
-    WeaponType weapon;
-
-    public WeaponCard(WeaponType weapon){
-        this.weapon = weapon;
+    WeaponType weapon = null;
+    
+    /** Constructor */
+    public WeaponCard (WeaponType weapon){
+        this.weapon = weapon;   
     }
-
-
-
-    public void setWeapon(WeaponType weapon){
-        this.weapon = weapon;
-
+    
+    /** Setter */
+    public WeaponType setWeapon (WeaponType weapon){
+        this.weapon = weapon;   
     }
-
-    public WeaponType getWeapon(){
+    
+    /** Getter */
+    public WeaponType getWeapon() {
         return this.weapon;
     }
 
-    /* Deletes selected weapon */
-    public void delete(){
-
+    /** Remove */
+    public WeaponType removeWeapon() {
+        this.weapon = null;
     }
 
+    /** Equals */
+    public boolean equals(Object o){
+        //checking if o is null
+        if(o == null){
+            return false;
+        }
 
+        //checking if o is equal to this class
+        if(getClass() != o.getClass()){
+            return false;
+        }
 
-
+        WeaponCard other = (WeaponCard) o;
+        return this.weapon == other.weapon;
+    }
 }
