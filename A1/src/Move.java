4e7b5e18ba5daa930e218f6704dd0fb46e5f61e4
julfran <68882531+julfran@users.noreply.github.com>
2020-08-08 22:56:14 +1200

    Update Move.java

--- A1/src/Move.java
+++ A1/src/Move.java
@@ -5,4 +5,19 @@ public class Move implements Cell {
     public Boolean isValidMove() {
         return null;
     }
+
+    @Override
+    public int getXPos() {
+        return 0;
+    }
+
+    @Override
+    public int getYPos() {
+        return 0;
+    }
+
+    @Override
+    public Player getPlayerInRoom() {
+        return null;
+    }
 }
