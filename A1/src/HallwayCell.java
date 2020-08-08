6259059750380782397871a22ab29ecbbf93da76
julfran <68882531+julfran@users.noreply.github.com>
2020-08-08 16:57:34 +1200

    Update HallwayCell.java

--- A1/src/HallwayCell.java
+++ A1/src/HallwayCell.java
@@ -1,37 +1,38 @@
-
 public class HallwayCell implements Cell {
 
-	private int x;
-	private int y;
-	private Player player = null;
-	
-	public HallwayCell(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public boolean isOccupied() {
-		if (player != null) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public Player getPlayer() {
-		if (isOccupied()) {
-			return player;
-		} else {
-			return null;
-		}
-	}
-	
+    private int x;
+    private int y;
+    private Player player = null;
+
+    public HallwayCell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public boolean isOccupied() {
+        if (player != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Player getPlayer() {
+        if (isOccupied()) {
+            return player;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int getXPos() {
+        return x;
+    }
+
+    @Override
+    public int getYPos() {
+        return y;
+    }
 }
