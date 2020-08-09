d9914b1adf29c4b9f28fe44675880751082566d5
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-09 22:13:36 +1200

    Update WeaponType.java
    changed ROPER to ROPE

--- A1/src/WeaponType.java
+++ A1/src/WeaponType.java
@@ -6,6 +6,6 @@ public enum WeaponType {
     DAGGER      {public String toString(){return "Dagger";}},
     LEADPIPE    {public String toString(){return "Lead Pipe";}},
     REVOLVER    {public String toString(){return "Revolver";}},
-    ROPER       {public String toString(){return "Rope";}},
+    ROPE        {public String toString(){return "Rope";}},
     SPANNER     {public String toString(){return "Spanner";}},
 }
