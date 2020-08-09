82ef6dd5904306c1ea8e35ce136db2f6a777787c
julfran <68882531+julfran@users.noreply.github.com>
2020-08-09 23:47:36 +1200

    Update RoomType.java

--- A1/src/RoomType.java
+++ A1/src/RoomType.java
@@ -1,14 +1,14 @@
 /**
- * Room Enum
- */
+* RoomType Enum
+*/
 public enum RoomType {
-    BALLROOM     {public String toString(){return "A";}},
+    BALLROOM {public String toString(){return "A";}} ,
     CONSERVATORY {public String toString(){return "C";}},
-    BILLIARD     {public String toString(){return "B";}},
-    LIBRARY      {public String toString(){return "L";}},
-    STUDY        {public String toString(){return "S";}},
-    HALL         {public String toString(){return "H";}},
-    LOUNGE       {public String toString(){return "O";}},
-    DINING       {public String toString(){return "D";}},
-    KITCHEN      {public String toString(){return "K";}},
+    BILLIARDROOM {public String toString(){return "B";}} ,
+    LIBRARY {public String toString(){return "L";}} ,
+    STUDY {public String toString(){return "S";}} ,
+    HALL  {public String toString(){return "H";}},
+    LOUNGE  {public String toString(){return "O";}},
+    DININGROOM  {public String toString(){return "D";}},
+    KITCHEN  {public String toString(){return "K";}},
 }
