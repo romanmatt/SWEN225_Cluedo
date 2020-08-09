6e3fde22549322f72566ff683877a5a6859e13c0
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-09 20:44:22 +1200

    Update Player.java
    Minor changes for compatibility with other classes

--- A1/src/Player.java
+++ A1/src/Player.java
@@ -1,26 +1,48 @@
 import java.util.ArrayList;
 
 public class Player {
-    private CharacterType playersCharacter;
-    private RoomCell playersRoom;
+
+	
+	
+	private CharacterType playersCharacter;
+    private RoomType room;
     private boolean isStillPlaying;
-    private ArrayList <Card> playersHand = new ArrayList<>();
+    private ArrayList <Card> hand = new ArrayList<>();
+    private Cell cell;
     String notes = ""; //Any extra notes for the player
 
+  
+    
     /* Constructor for the Player object */
     public Player(CharacterType character){
         this.playersCharacter = character;
         isStillPlaying = true;
     }
+
     /* Helper methods */
 
-    public ArrayList<Card> getHand(){
-        return this.playersHand;
-    }
+    public int getX() {
+  		return cell.getX();
+  	}
 
+  	public int getY() {
+  		return cell.getY();
+  	}
+
+  	public Cell getCell() {
+  		return cell;
+  	}
+
+  	public RoomType getRoom() {
+  		return room;
+  	}
 
     public CharacterType getName(){
         return this.playersCharacter;
+    }
+    
+    public ArrayList<Card> getHand() {
+    	return hand;
     }
 
 
@@ -38,15 +60,4 @@ public class Player {
         return this.isStillPlaying;
     }
 
-
-
-
-
-
-
-
-
 }
-
-
-
