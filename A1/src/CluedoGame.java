f0f39edec7e1d036d4300338f1e6f41e205a3b23
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-09 23:01:15 +1200

    Update CluedoGame.java

--- A1/src/CluedoGame.java
+++ A1/src/CluedoGame.java
@@ -220,6 +220,76 @@ public class CluedoGame {
      * draws the board into console
      * */
 
+    private void setupCards() {
+    	ArrayList<Card> remainingDeck = new ArrayList<Card>();
+    	ArrayList<RoomCard> roomDeck = new ArrayList<RoomCard>();
+    	roomDeck.add(new RoomCard(RoomType.BALLROOM));
+    	roomDeck.add(new RoomCard(RoomType.CONSERVATORY));
+    	roomDeck.add(new RoomCard(RoomType.BILLIARD));
+    	roomDeck.add(new RoomCard(RoomType.LIBRARY));
+    	roomDeck.add(new RoomCard(RoomType.STUDY));
+    	roomDeck.add(new RoomCard(RoomType.HALL));
+    	roomDeck.add(new RoomCard(RoomType.LOUNGE));
+    	roomDeck.add(new RoomCard(RoomType.DINING));
+    	roomDeck.add(new RoomCard(RoomType.KITCHEN));
+    	RoomCard murderRoom = roomDeck.get(new Random().nextInt(roomDeck.size()));
+    	roomDeck.remove(murderRoom);
+    	solutionRoom = murderRoom.getRoom();
+    	remainingDeck.addAll(roomDeck);
+    	ArrayList<WeaponCard> weaponDeck = new ArrayList<WeaponCard>();
+    	weaponDeck.add(new WeaponCard(WeaponType.CANDLESTICK));
+    	weaponDeck.add(new WeaponCard(WeaponType.DAGGER));
+    	weaponDeck.add(new WeaponCard(WeaponType.LEADPIPE));
+    	weaponDeck.add(new WeaponCard(WeaponType.REVOLVER));
+    	weaponDeck.add(new WeaponCard(WeaponType.ROPE));
+    	weaponDeck.add(new WeaponCard(WeaponType.SPANNER));
+    	WeaponCard murderWeapon = weaponDeck.get(new Random().nextInt(weaponDeck.size()));
+    	weaponDeck.remove(murderWeapon);
+    	solutionWeapon = murderWeapon.getWeapon();
+    	remainingDeck.addAll(weaponDeck);
+    	ArrayList<CharacterCard> characterDeck = new ArrayList<CharacterCard>();
+    	characterDeck.add(new CharacterCard(CharacterType.MISSSCARLET));
+    	characterDeck.add(new CharacterCard(CharacterType.COLONELMUSTARD));
+    	characterDeck.add(new CharacterCard(CharacterType.MRSWHITE));
+    	characterDeck.add(new CharacterCard(CharacterType.MRGREEN));
+    	characterDeck.add(new CharacterCard(CharacterType.MRSPEACOCK));
+    	characterDeck.add(new CharacterCard(CharacterType.PROFESSORPLUM));
+    	CharacterCard murderer = characterDeck.get(new Random().nextInt(characterDeck.size()));
+    	characterDeck.remove(murderer);
+    	solutionCharacter = murderer.getCharacter();
+    	remainingDeck.addAll(characterDeck);
+    	while (!remainingDeck.isEmpty()) {
+    		for (Player p : activePlayers) {
+    			if (!remainingDeck.isEmpty()) {
+    				p.addToHand(remainingDeck.get(new Random().nextInt(remainingDeck.size())));
+    			}
+    		}
+    	}
+    }
+    
+    public void setupPlayers() {
+    	for (Player p : activePlayers) {
+    		if (p.getName() == CharacterType.MISSSCARLET) {
+    			((HallwayCell) board.board[7][23]).placePlayer(p);
+    		}
+    		if (p.getName() == CharacterType.COLONELMUSTARD) {
+    			((HallwayCell) board.board[0][17]).placePlayer(p);
+    		}
+    		if (p.getName() == CharacterType.MRSWHITE) {
+    			((HallwayCell) board.board[9][0]).placePlayer(p);
+    		}
+    		if (p.getName() == CharacterType.MRGREEN) {
+    			((HallwayCell) board.board[14][0]).placePlayer(p);
+    		}
+    		if (p.getName() == CharacterType.MRSPEACOCK) {
+    			((HallwayCell) board.board[24][7]).placePlayer(p);
+    		}
+    		if (p.getName() == CharacterType.PROFESSORPLUM) {
+    			((HallwayCell) board.board[24][19]).placePlayer(p);
+    		}
+    	}
+    }
+    
     public static void main(String[] args){
         CluedoGame cluedoGame = new CluedoGame();
         cluedoGame.run();
