3b1b571332f51b750d659d291f7df6452a12048d
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-09 13:21:58 +1200

    Update Board.java
    Began work on the canMove() pathfinding method

--- A1/src/Board.java
+++ A1/src/Board.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Board {
     private Cell[][] board;
     Room ballroom;
@@ -11,31 +13,32 @@ public class Board {
     Room kitchen;
 
     String initialCellBoard =
-                    ".........W....G........./n" +
-                    "KKKKKK....AAAA....CCCCCC/n" +
-                    "KKKKKK..AAAAAAAA..CCCCCC/n" +
-                    "KKKKKK..AAAAAAAA..CCCCCC/n"+
-                    "KKKKKK..AAAAAAAA..+CCCCC/n"+
-                    "KKKKKK..+AAAAAA+...CCCC./n"+
-                    ".KKKK+..AAAAAAAA......../n"+
-                    "........A+AAAA+A.......E/n"+
-                    "..................BBBBBB/n"+
-                    "DDDDDDDD..........+BBBBB/n"+
-                    "DDDDDDDD..........BBBBBB/n"+
-                    "DDDDDDD...........BBBBBB/n"+
-                    "DDDDDDDD..........BBBB+B/n"+
-                    "DDDDDDDD................/n"+
-                    "DDDDDDDD..........LL+LL./n"+
-                    "DDDDDD+D.........LLLLLLL/n"+
-                    ".................+LLLLLL/n"+
-                    "M................LLLLLLL/n"+
-                    ".........HH++HH...LLLLL./n"+
-                    "OOOOOO+..HHHHHH........P/n"+
-                    "OOOOOOO..HHHHH+........./n"+
-                    "OOOOOOO..HHHHHH..+SSSSSS/n"+
-                    "OOOOOOO..HHHHHH..SSSSSSS/n"+
-                    "OOOOOOO..HHHHHH..SSSSSSS/n"+
-                    "OOOOOO.R.HHHHHH...SSSSSS/n";
+
+            ".........W....G........." +
+            "KKKKKK....AAAA....CCCCCC" +
+            "KKKKKK..AAAAAAAA..CCCCCC" +
+            "KKKKKK..AAAAAAAA..CCCCCC"+
+            "KKKKKK..AAAAAAAA..+CCCCC"+
+            "KKKKKK..+AAAAAA+...CCCC."+
+            ".KKKK+..AAAAAAAA........"+
+            "........A+AAAA+A.......E"+
+            "..................BBBBBB"+
+            "DDDDDDDD..........+BBBBB"+
+            "DDDDDDDD..........BBBBBB"+
+            "DDDDDDD...........BBBBBB"+
+            "DDDDDDDD..........BBBB+B"+
+            "DDDDDDDD................"+
+            "DDDDDDDD..........LL+LL."+
+            "DDDDDD+D.........LLLLLLL"+
+            ".................+LLLLLL"+
+            "M................LLLLLLL"+
+            ".........HH++HH...LLLLL."+
+            "OOOOOO+..HHHHHH........P"+
+            "OOOOOOO..HHHHH+........."+
+            "OOOOOOO..HHHHHH..+SSSSSS"+
+            "OOOOOOO..HHHHHH..SSSSSSS"+
+            "OOOOOOO..HHHHHH..SSSSSSS"+
+            "OOOOOO.R.HHHHHH...SSSSSS";
 
     public final String draw() {
         return null;
@@ -157,12 +160,61 @@ public class Board {
     private Cell parseBallroom(int x, int y) {
         return new RoomCell(x, y, RoomType.BALLROOM, this);
     }
+    
+    public Cell getCell(int x, int y) {
+    	return board[x][y];
+    }
 
     // Checks if a player can make their desired move
     // player is the player wishing to move. Their position on the board is taken from this input
     // finish is the cell they wish to end their move on
     // roll is the number they rolled on the dice
     public Boolean canMove(Player player, Cell finish, int roll) {
-        return false;
+    	// Step one: Determine the player's starting cell
+    	// Default case: hallway
+    	Cell pos = player.getCell();
+    	// Alt: Room
+    	if (player.getRoom() != null) {
+    		// Step 1: Figure out which room
+    		RoomType room = player.getRoom();
+    		// Step 2: Determine which door to leave from
+    		pos = closestDoor(room, finish);
+    	}
+    	// Step Two: Determine the actual finish cell
+    	// Default case: Hallway
+    	Cell target = finish;
+    	// Alt: Room/Door
+    	if (finish.getClass() != HallwayCell.class) {
+    		// Choose the closest door to the chosen room
+			target = closestDoor(finish.getType(), pos);
+    	}
+    	// Step Three: find path
+    	// Best case scenario: corner room to opposing corner room
+    	// Checked here since it counts as a whole move, so the player's dice roll can't have had any affect yet
+    	if (pos.getClass() == RoomCell.class || pos.getClass() == DoorCell.class) {
+    		if (target.getClass() == RoomCell.class || target.getClass() == DoorCell.class) {
+    			if (pos.getType() == RoomType.KITCHEN && target.getType() == RoomType.LIBRARY) {
+    	    		return true;
+    	    	}
+    	    	if (target.getType() == RoomType.KITCHEN && pos.getType() == RoomType.LIBRARY) {
+    	    		return true;
+    	    	}
+    	    	if (pos.getType() == RoomType.STUDY && target.getType() == RoomType.BILLIARD) {
+    	    		return true;
+    	    	}
+    	    	if (target.getType() == RoomType.STUDY && pos.getType() == RoomType.BILLIARD) {
+    	    		return true;
+    	    	}
+    		}
+    	}
+    	// Alt: Path finding
+    	ArrayList<Cell> path = new ArrayList<Cell>();
+    	// TODO continue from here
     }
+    
+    // Should contain the coords of the doors of each room, and calculates which of those is closer to the given target cell
+    private Cell closestDoor(RoomType room, Cell target) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 }
