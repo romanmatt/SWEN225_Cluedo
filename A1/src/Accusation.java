baf9216048718dfe9c4257040b9e38f1b07c3fff
Scott <68895106+LoadingReadyWalk@users.noreply.github.com>
2020-08-09 14:12:19 +1200

    Update Accusation.java

--- A1/src/Accusation.java
+++ A1/src/Accusation.java
@@ -2,30 +2,32 @@ public class Accusation {
     public RoomType accusedRoom;
     public WeaponType accusedWeapon;
     public CharacterType accusedCharacter;
+    private Player player;
 
 
     /* Constructor for Accusation object */
-    public Accusation(RoomType accusedRoom, WeaponType accusedWeapon, CharacterType accusedCharacter){
+    public Accusation(RoomType accusedRoom, WeaponType accusedWeapon, CharacterType accusedCharacter, Player player){
         this.accusedRoom = accusedRoom;
         this.accusedWeapon = accusedWeapon;
         this.accusedCharacter = accusedCharacter;
+        this.player = player;
     }
 
-
-public boolean checkAccusation(Player player){
-        for(int i = 0; i < 6; i++){
-
-            //this is temporary
-            System.out.println();
-            player.setIsStillPlaying(false);
-            System.out.println("Player " + player.getName() + " is out!");
-            return false;
-
-        }
-
-
-        return false;
-}
+// Moved this method to CluedoGame to have access to the solution
+//public boolean checkAccusation(){
+//        for(int i = 0; i < 6; i++){
+//
+//            //this is temporary
+//            System.out.println();
+//            player.setIsStillPlaying(false);
+//            System.out.println("Player " + player.getName() + " is out!");
+//            return false;
+//
+//        }
+//
+//
+//        return false;
+//}
 
 
     public String toString() {
