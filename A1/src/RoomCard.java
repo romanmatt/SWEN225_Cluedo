b41f97b0bf6dd97265d00fb87eb8e796a79fb6ea
riverarive <38926963+riverarive@users.noreply.github.com>
2020-08-10 15:04:21 +1200

    Update RoomCard.java

--- A1/src/RoomCard.java
+++ A1/src/RoomCard.java
@@ -43,6 +43,33 @@ class RoomCard implements Card {
     
     /** To String */
     public String toString(){
-        
+        if(getRoom() instanceof RoomType.BALLROOM){
+            return "Ballroom";
+        }
+        if(getRoom() instanceof RoomType.CONSERVATORY){
+            return "Conservatory";
+        }
+        if(getRoom() instanceof RoomType.BILLIARDROOM){
+            return "Billiard Room";
+        }
+        if(getRoom() instanceof RoomType.LIBRARY){
+            return "Library";
+        }
+        if(getRoom() instanceof RoomType.STUDY){
+            return "Study";
+        }
+        if(getRoom() instanceof RoomType.HALL){
+            return "Hall";
+        }
+        if(getRoom() instanceof RoomType.LOUNGE){
+            return "Lounge";
+        }
+        if(getRoom() instanceof RoomType.DININGROOM){
+            return "Lounge";
+        }
+        if(getRoom() instanceof RoomType.KITCHEN){
+            return "Lounge";
+        }
+           
     }
 }
