5c45a68120e2219bce0cf75ee9ae67ff411a4d5f
riverarive <38926963+riverarive@users.noreply.github.com>
2020-08-24 00:16:23 +1200

    Update GUI.java

--- A1/src/GUI.java
+++ A1/src/GUI.java
@@ -11,14 +11,25 @@ public abstract class GUI {
     
     //Suggest Button  
     JButton suggest = new JButton("Suggest");
+    suggest.addActionListener(new ActionListener(){
+	    
+    });
     //---------------------------------------------------------------------------------
 	  
     //Accuse Button	  
     JButton accuse = new JButton("Accuse");
+    accuse.addActionListener(new ActionListener(){
+	    
+    });
     //---------------------------------------------------------------------------------
 	  
     //Quit Button	  
     JButton quit = new JButton("Quit");
+    quit.addActionListener(new ActionListener(){
+    	public void actionPerformed(ActionEvent event){
+		System.exit(0);
+	}
+    });
     //---------------------------------------------------------------------------------
   }
   
