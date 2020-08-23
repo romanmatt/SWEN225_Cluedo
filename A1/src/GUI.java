3083f31c147faea03c2121b9cb65f3271696e550
riverarive <38926963+riverarive@users.noreply.github.com>
2020-08-24 00:19:26 +1200

    Update GUI.java

--- A1/src/GUI.java
+++ A1/src/GUI.java
@@ -8,21 +8,39 @@ public abstract class GUI {
 	}
   
   private void initialise(){
-    
+	  
+    //BUTTONS	  
+    //---------------------------------------------------------------------------------
+    //Start Game Button	  
+    JButton start = new JButton("Start");
+    start.addActionListener(new ActionListener(){
+	    
+    });
+    //---------------------------------------------------------------------------------
+    //Move Button	  
+    JButton start = new JButton("Move");
+    move.addActionListener(new ActionListener(){
+	    
+    });	  
+    //---------------------------------------------------------------------------------	  
     //Suggest Button  
     JButton suggest = new JButton("Suggest");
     suggest.addActionListener(new ActionListener(){
 	    
     });
-    //---------------------------------------------------------------------------------
-	  
+    //---------------------------------------------------------------------------------  
     //Accuse Button	  
     JButton accuse = new JButton("Accuse");
     accuse.addActionListener(new ActionListener(){
 	    
     });
     //---------------------------------------------------------------------------------
-	  
+    //End Turn Button	  
+    JButton end = new JButton("End");
+    end.addActionListener(new ActionListener(){
+	    
+    });	  
+    //---------------------------------------------------------------------------------	  
     //Quit Button	  
     JButton quit = new JButton("Quit");
     quit.addActionListener(new ActionListener(){
